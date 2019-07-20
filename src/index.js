c3e1c1e1025a55e6f2dc32d241813a92801ec44b
Pawel Gmyrek <(pawelgmyrek@yahoo.com)>
2019-07-19 22:06:31 -0400

    forked auth

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
 
 module.hot.accept();
